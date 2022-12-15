66b96fe1ee40f64a812be35702a872e0b41eb0e7
AlejandroBojaca <alejandrobojaca1@gmail.com>
2022-12-15 23:52:25 +0300

    🔨fix: modal buttons

--- components/ModalComponent.vue
+++ components/ModalComponent.vue
@@ -5,21 +5,25 @@ const mainStore = useMainStore();
 const onSubmit = () => {
   mainStore.toggleShowModal();
 }
+
+const readFiles = (e) =>{
+  console.log(e.target.files);
+}
 </script>
 
 <template>
   <div>
     <div class="flex flex-col items-center justify-center w-screen h-screen fixed top-0 left-0 bg-black opacity-75">
       <div  class="modal-container">
-        <div class="main-text mb-8 pb-4">
+        <div class="main-text mb-4 pb-2">
           <h1 class="text-white text-2xl font-bold ">Select Your File Type</h1>
         </div>
-  
-        <div class="mt-4 flex flex-row space-x-8 w-24">
-          <button type="submit" class="text-white button py-2 px-4">DEF file</button>
-          <button type="submit" class="text-white button py-2 px-4">LEF file</button>  
+        <div class="flex flex-row space-x-8 w-24">
+          <input @change="readFiles" type="file" name="file" id="def-file" class="inputfile" />
+          <label for="def-file" id="left-button" class="button py-2 px-4 mt-4" >DEF file</label>
+          <input type="file" name="file" id="lef-file" class="inputfile" />
+          <label for="lef-file" class="button py-2 px-4 mt-4">LEF file</label>  
         </div>
-  
        <button
         @click="onSubmit()" 
         type="submit"  
@@ -44,9 +48,15 @@ const onSubmit = () => {
     border-radius: 10px;
   }
 
+  #left-button{
+    margin-left: 0;
+  }
+
   button:hover {
     transition-duration: 100ms;
     background-color: #D3D3D3;
+    color: black;
+    border: 1px solid black;
   }
   
   .modal-container{
@@ -54,6 +64,29 @@ const onSubmit = () => {
     padding: 25px;
     border-radius: 15px;
   }
+
+  .inputfile {
+	width: 0.1px;
+	height: 0.1px;
+	opacity: 0;
+	overflow: hidden;
+	position: absolute;
+	z-index: -1;
+}
+
+.inputfile + label {
+  color: white;
+  display: inline-block;
+}
+
+.inputfile:focus + label,
+.inputfile + label:hover {
+  cursor: pointer;
+  transition-duration: 100ms;
+  background-color: #D3D3D3;
+  color: black;
+  border: 1px solid black;
+}
   
 
 </style>
